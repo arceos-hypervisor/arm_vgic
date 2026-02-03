ba3d8157df8dc33f9e22534e83758ddc68462729
周睿 <zrufo747@outlook.com>
2026-02-03 13:55:50 +0800

    Remove VGIC and related timer implementations
    - Deleted VGICD, VGICR, and Vgic structures along with their associated methods for handling MMIO reads and writes.
    - Removed utility functions for MMIO operations from utils.rs.
    - Eliminated timer-related structures and their implementations, including SysCntpCtlEl0, SysCntpctEl0, and SysCntpTvalEl0.
    - Cleaned up the vtimer module by removing references to the deleted timer structures.

--- src/v3/mod.rs
+++ src/v3/mod.rs
@@ -1,5 +1,24 @@
-pub mod gits;
-mod registers;
-mod utils;
-pub mod vgicd;
-pub mod vgicr;
+use vdev_if::MmioRegion;
+pub use vdev_if::VirtPlatformOp;
+pub use vdev_if::{GuestPhysAddr, VirtDeviceOp};
+
+pub mod icc;
+
+pub struct VGic<P: VirtPlatformOp> {
+    gicd: MmioRegion,
+    plat: P,
+}
+
+impl<P: VirtPlatformOp> VGic<P> {
+    pub fn new(gicd: GuestPhysAddr, gicr: GuestPhysAddr, plat: P) -> Self {
+        let gicd = plat.alloc_mmio_region(Some(gicd), 0x10000, false).unwrap();
+
+        Self { plat, gicd }
+    }
+}
+
+impl<P: VirtPlatformOp> VirtDeviceOp for VGic<P> {
+    fn name(&self) -> &str {
+        "GICv3 distributor"
+    }
+}
